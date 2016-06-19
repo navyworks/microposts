55e495e9a9ce2f3e513cb47732a7af27de271181
NavyWorks <navycolor03@gmail.com>
2016-06-19 15:32:16 +0000

    add profile edit&update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
 
   def show
    @user = User.find(params[:id])
@@ -11,6 +15,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+  	  log_in @user
   	  flash[:success] = "Welcome to the Sample App!"
   	  redirect_to @user
   	else
@@ -18,10 +23,45 @@ class UsersController < ApplicationController
   	end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    if @user.update_attributes(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'プロフィールを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+
+
   private
 
   def user_params
-  	params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "ログインしてください"
+      redirect_to login_url
+    end
+  end
+
+	# 正しいユーザーかどうか確認
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_url) unless current_user?(@user)
+	end
+
 end
