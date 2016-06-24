10c67a7e324645513d644e67066cef304649ed96
NavyWorks <navycolor03@gmail.com>
2016-06-24 11:23:10 +0000

    update form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,21 @@
 class SessionsController < ApplicationController
-	def new
-	end
+  def new
+  end
 
-	def create
-		@user = User.find_by(email: params[:session][:email].downcase)
-		if @user && @user.authenticate(params[:session][:password])
-			log_in @user
-			redirect_back_or @user
-		else
-			flash.now[:danger] = 'E-mail/パスワードを確認し再度入力してください。'
-			render 'new'
-		end
-	end
-	
+  def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      redirect_back_or @user
+    else
+      flash.now[:danger] = 'E-mail/パスワードを確認し再度入力してください。'
+      render 'new'
+    end
+  end
+  
 
-	def destroy
-		session[:user_id] = nil
-		redirect_to root_path
-	end
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
 end
